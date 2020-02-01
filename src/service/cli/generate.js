6fef269ec3cd408f7f47b728b0ffc92d51a2f371
Lana Kostina <exxlain@gmail.com>
2020-02-01 12:17:43 +0700

    changed data format to txt

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -9,14 +9,15 @@ const {
 const {
   DEFAULT_COUNT,
   FILE_NAME,
-  TITLES,
-  SENTENCES,
-  CATEGORIES,
   Messages,
   FullTextRestrict,
   AnnonceTextRestrict,
+  CategoriesRestrict,
   EXIT_CODE_FAILURE,
   MAX_OFFERS_NUMBER,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH,
 } = require(`./constants`);
 
 const getCreatedDate = () =>{
@@ -27,31 +28,46 @@ const getCreatedDate = () =>{
   return new Date(getRandomInt(threeMonthAgoTimestamp, currentDateTimestamp));
 };
 
-const generateOffers = (count) => (
+const generateOffers = (count, sentences, titles, categories) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: getCreatedDate(),
-    announce: shuffle(SENTENCES).slice(0, getRandomInt(FullTextRestrict.min, FullTextRestrict.max)).join(` `),
-    fullText: shuffle(SENTENCES).slice(0, getRandomInt(AnnonceTextRestrict.min, AnnonceTextRestrict.max)).join(` `),
-    categories: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+    announce: shuffle(sentences).slice(0, getRandomInt(FullTextRestrict.min, FullTextRestrict.max)).join(` `),
+    fullText: shuffle(sentences).slice(0, getRandomInt(AnnonceTextRestrict.min, AnnonceTextRestrict.max)).join(` `),
+    categories: shuffle(categories).slice(0, getRandomInt(CategoriesRestrict.min, CategoriesRestrict.max)).join(`, `),
   }))
 );
+
+const readContent = async (filePath) => {
+  try {
+    const initialContent = await fs.readFile(filePath, `utf8`);
+    const content = initialContent.trim();
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(`${Messages.readingError} ${filePath}`));
+    return process.exit(EXIT_CODE_FAILURE);
+  }
+};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     if (countOffer > MAX_OFFERS_NUMBER) {
       console.info(chalk.red(Messages.overmuch));
       process.exit(EXIT_CODE_FAILURE);
     }
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, sentences, titles, categories));
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(Messages.success));
     } catch (error) {
-      console.error(chalk.red(Messages.error));
+      console.error(chalk.red(Messages.writingError));
       process.exit(EXIT_CODE_FAILURE);
     }
   }
