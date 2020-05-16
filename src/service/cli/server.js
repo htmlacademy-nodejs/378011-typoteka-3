ed031ae6fea88c7792398cfce3a2a8a66f13a436
Svetlana Kostina <kostina@sibext.com>
2020-05-16 17:30:47 +0700

    [added] express server for service

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,67 +1,33 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
-const fs = require(`fs`).promises;
+const express = require(`express`);
+const postsRoutes = require(`../routes/posts`);
+
 const {
   DEFAULT_PORT,
-  FILE_NAME,
   HttpCode,
-  Messages,
-  ROOT_PAGE_PATH,
-  EXIT_CODE_FAILURE,
 } = require(`./constants`);
 
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case ROOT_PAGE_PATH:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, Messages.NOT_FOUND);
-      }
+const app = express();
+app.use(express.json());
 
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, Messages.NOT_FOUND);
-      break;
-  }
-};
+app.use(`/posts`, postsRoutes);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
+app.use((req, res) => res
+.status(HttpCode.NOT_FOUND)
+.send(`Not found`));
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          console.error(Messages.SERVER_CREATION_ERROR, err);
-          return process.exit(EXIT_CODE_FAILURE);
-        }
-
-        return console.info(chalk.green(`${Messages.WAITING_CONNECTION} ${port}`));
-      });
+    app.listen(port, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`, err);
+      }
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    });
   }
 };
